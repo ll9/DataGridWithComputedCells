05106766f3f18b56fcd23074b8e645fdbe7e9129
ll9 <llwierer@web.de>
2018-12-17 21:21:10 +0100

    Basic Repository

---
+++ DataGridWithComputedCells/Repositories/PersonRepository.cs
@@ -0,0 +1,54 @@
+﻿using DataGridWithComputedCells.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridWithComputedCells.Repositories
+{
+    class PersonRepository
+    {
+        private readonly DbContext _context;
+        private readonly string _tableName = "Person";
+
+        public PersonRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public void Fill (DataTable dataTable)
+        {
+            var query = $"SELECT *, MAX(ROWID) AS MAX_ROWID FROM {_tableName}";
+
+            using (var connection = _context.GetConnection())
+            using (var adapter = new SQLiteDataAdapter(query, connection))
+            {
+                var builder = new SQLiteCommandBuilder(adapter);
+                adapter.UpdateCommand = builder.GetUpdateCommand();
+                adapter.DeleteCommand = builder.GetDeleteCommand();
+                adapter.InsertCommand = builder.GetInsertCommand();
+
+                adapter.Fill(dataTable);
+            }
+        }
+
+        public void Update(DataTable dataTable)
+        {
+            var query = $"SELECT *, MAX(ROWID) AS MAX_ROWID FROM {_tableName}";
+
+            using (var connection = _context.GetConnection())
+            using (var adapter = new SQLiteDataAdapter(query, connection))
+            {
+                var builder = new SQLiteCommandBuilder(adapter);
+                adapter.UpdateCommand = builder.GetUpdateCommand();
+                adapter.DeleteCommand = builder.GetDeleteCommand();
+                adapter.InsertCommand = builder.GetInsertCommand();
+
+                adapter.Update(dataTable);
+            }
+        }
+    }
+}
