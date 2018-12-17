571cdf00052949635a630720e2776cac67a97fed
ll9 <llwierer@web.de>
2018-12-17 21:12:03 +0100

    DbContext

---
+++ DataGridWithComputedCells/Data/DbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridWithComputedCells.Data
+{
+    public class DbContext
+    {
+        private readonly string _connectionString;
+
+        public DbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            var connection = new SQLiteConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
